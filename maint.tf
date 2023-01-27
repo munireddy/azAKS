25010b2b48a3b161dd3b51eb09b1e5fbb888adac
munireddy <munisekhar.reddy@gmail.com>
2023-01-27 06:56:55 -0500

    Added AKS Cluster resources

--- maint.tf
+++ maint.tf
@@ -17,6 +17,24 @@ resource "azurerm_resource_group" "rg" {
   location = "EastUS2"
 }
 
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "azakscluster01"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
     command = "echo 'Sample to test Terraform on Azure'"
