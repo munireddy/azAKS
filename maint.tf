377efbe35ca67458b42a6d67fad1fa5b3e3b6773
munireddy <munisekhar.reddy@gmail.com>
2023-01-27 07:24:14 -0500

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "learnk8scluster"
+  name                = "azakscluster01"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "azakscluster01"
